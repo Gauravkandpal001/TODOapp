cea1fd888b24306efdd5d9a8f317685e200440d0
Gaurav <kandpalgaurav241@gmail.com>
2018-03-29 21:52:41 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+﻿var express=require('express');
+var todoController= require('./controllers/todoController');
+
+var app=express();
+
+
+//set up template engine
+app.set('view engine','ejs');
+
+
+//static fileSize
+app.use(express.static('public'));
+
+//fire controllers
+todoController(app);
+
+//listen to port
+app.listen('3000');
+//console.log('you are listening to port 3000');
---
+++ controllers/todoController.js
@@ -0,0 +1,48 @@
+﻿var bodyParser =require('body-parser');
+var mongoose=require('mongoose');
+//connect to the database
+
+mongoose.connect('mongodb://test:test@ds157097.mlab.com:57097/todo');
+
+
+//create a schema -this is like a blueprint
+var todoSchema =new mongoose.Schema({
+	item: String
+});
+
+//createa model type of todo
+var Todo= mongoose.model('Todo',todoSchema);
+
+
+//var data=[{item:'get milk'},{item:'walk dog'},{item:'kick some coading ass'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+
+module.exports= function(app){
+
+	 app.get('/',function(req,res){
+		 //get data from mongodb and pass it to the view
+		 Todo.find({},function(err,data){
+			 if(err)throw err;
+			 res.render('todo',{todos:data});
+		 });
+
+	 });
+	 app.post('/',urlencodedParser,function(req,res){
+		 //get data from the view and add it to mongodb var
+		 var newTodo=Todo(req.body).save(function(err,data){
+			if(err)throw err;
+            res.json(data);
+		 })
+
+	 });
+	 app.delete('/:item',function(req,res){
+		 //delete the requested item from mongodb
+		 Todo.find({item:req.params.item.replace(/\-/g," ")}).remove(function(err,data){
+			 if(err)throw err;
+			 res.json(data);
+		 })
+
+	 });
+
+};
